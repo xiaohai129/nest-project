648bad595f63fb2b4dee3c64825fb957af1b7e9b
xh <2541448787@qq.com>
2021-03-03 10:37:16 +0800

    fix: crud

--- src/libs/crud/crud.service.ts
+++ src/libs/crud/crud.service.ts
@@ -1,38 +1,4 @@
-import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
 
-export class CrudService<T> {
-  constructor(private readonly entity: Repository<T>) {}
-
-  create(params: T) {
-    return this.entity.insert(params);
-  }
-
-  async get(id: number) {
-    console.log(id);
-
-    return await this.entity.findOne(id);
-  }
-
-  async delete(id: number) {
-    const res = await this.entity.delete(id);
-    return res.raw.id;
-  }
-
-  async update(params: T) {
-    const pid = params['id'];
-    delete params['id'];
-    await this.entity.update(pid, params);
-    return pid;
-  }
-
-  async getList(options: any) {
-    const res = await this.entity.findAndCount({
-      skip: options.pageNum * options.pageSize,
-      take: options.pageSize
-    });
-    return {
-      total: res[1] as number,
-      list: res[0] as T[]
-    };
-  }
-}
+@Injectable()
+export class CrudService {}
--- src/libs/crud/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './crud.decorator';
-export * from './crud.service';
